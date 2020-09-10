69dc71d57e3324c8d8affc37ff0e336bc8e1092c
percevil <peeevil@gmail.com>
2020-09-09 17:22:10 -0700

    house cleanig and tweaks

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 const todolist = document.querySelector('.todolist');
 const taskList = todolist.querySelector('.todolist__tasks');
-// removing task form list
 
-//
+// Deleting tasks
 todolist.addEventListener('click', event => {
-  // addd event listener
+  // add event listener
   if (!event.target.matches('.task__delete-button')) return;
   // if the event matches .task__delete-button the continue else
   // stop action
@@ -13,7 +12,7 @@ todolist.addEventListener('click', event => {
   const taskElement = event.target.parentElement;
   // finds parent element
   taskList.removeChild(taskElement);
-  // removes child
+  // removes child (task)
 
   // Triggers empty state
   if (taskList.children.length === 0) {
@@ -26,25 +25,30 @@ todolist.addEventListener('click', event => {
 todolist.addEventListener('submit', event => {
   event.preventDefault();
 
-  // get value of the task
+  // get value of the task input
   const newTaskField = todolist.querySelector('input');
-  
+
   const inputValue = newTaskField.value.trim();
   // trims whitespace from field input
-  
+
   // Add a new task
   const taskElement = document.createElement('li');
   taskElement.classList.add('task');
+  // collects what the user has typed
   taskElement.innerHTML = `<input type="checkbox" id="task-1" />
   <label for="task-1"> ... </label>
   <span class="task__name">${inputValue}</span>
   <button type="button" class="task__delete-button">
     <svg viewBox="0 0 20 20"> ... </svg>
   </button>`;
+  if (!inputValue) return;
+  // Turns off the Listener so empty tasks cant be entered unless there is something in the input field.
   taskList.appendChild(taskElement);
+  // appends child to th DOM , which makes the task visible
   newTaskField.value = '';
-  
-
+  // clears input field for another task , so user doesnt have to clear it manually
+  newTaskField.focus();
+  // Adds focus to input after user enters a new task (blinking cursor)
   // Sanitize the innerHTML
   taskElement.innerHTML = DOMPurify.sanitize(`
 <input type="checkbox" id="task" />
@@ -71,9 +75,9 @@ todolist.addEventListener('submit', event => {
     <svg viewBox="0 0 20 20"> ... </svg>
   </button>
   `);
-
-  
 });
+
+//  API stuff
 
 // const rootendpoint = 'https://api.learnjavascript.today';
 
